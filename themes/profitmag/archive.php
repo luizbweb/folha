f54cad403be74eedd174a1c6344230394e7915ee
Carlos1545 <carlos.jr1545@gmail.com>
2015-07-20 19:02:55 -0300

    Tradução archive.php

--- themes/profitmag/archive.php
+++ themes/profitmag/archive.php
@@ -34,16 +34,16 @@ if( $sidebar_layout == 'both_sidebar' ) {
 							single_tag_title();
 
 						elseif ( is_author() ) :
-							printf( __( 'Author: %s', 'profitmag' ), '<span class="vcard">' . get_the_author() . '</span>' );
+							printf( __( 'Autor: %s', 'profitmag' ), '<span class="vcard">' . get_the_author() . '</span>' );
 
 						elseif ( is_day() ) :
-							printf( __( 'Day: %s', 'profitmag' ), '<span>' . get_the_date() . '</span>' );
+							printf( __( 'Dia: %s', 'profitmag' ), '<span>' . get_the_date() . '</span>' );
 
 						elseif ( is_month() ) :
-							printf( __( 'Month: %s', 'profitmag' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'profitmag' ) ) . '</span>' );
+							printf( __( 'Mês: %s', 'profitmag' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'profitmag' ) ) . '</span>' );
 
 						elseif ( is_year() ) :
-							printf( __( 'Year: %s', 'profitmag' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'profitmag' ) ) . '</span>' );
+							printf( __( 'Ano: %s', 'profitmag' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'profitmag' ) ) . '</span>' );
 
 						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
 							_e( 'Asides', 'profitmag' );
