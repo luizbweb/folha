5f02c36b8ed42bf21be98c99255c9444bd319a47
Carlos1545 <carlos.jr1545@gmail.com>
2015-07-20 17:34:11 -0300

    Tradução da página que não evidencia nenhum conteúdo

--- themes/profitmag/content-none.php
+++ themes/profitmag/content-none.php
@@ -10,22 +10,22 @@
 
 <section class="no-results not-found">
 	<header class="page-header">
-		<h1 class="page-title"><?php _e( 'Nothing Found', 'profitmag' ); ?></h1>
+		<h1 class="page-title"><?php _e( 'Nada Encontrado', 'profitmag' ); ?></h1>
 	</header><!-- .page-header -->
 
 	<div class="page-content">
 		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'profitmag' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+			<p><?php printf( __( 'Preparado para publicar sua primeira postagem? <a href="%1$s">Comece aqui</a>.', 'profitmag' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
 
 		<?php elseif ( is_search() ) : ?>
 
-			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'profitmag' ); ?></p>
+			<p><?php _e( 'Desculpe, mas nada foi encontrado em seus termos de pesquisa. Por favor, tente novamente com palavras-chave diferentes.', 'profitmag' ); ?></p>
 			
 
 		<?php else : ?>
 
-			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'profitmag' ); ?></p>
+			<p><?php _e( 'Parece que nós não podemos encontrar o que você está buscando. Tente buscar mais uma vez.', 'profitmag' ); ?></p>
 			
 
 		<?php endif; ?>
